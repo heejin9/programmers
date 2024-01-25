a18a0830753a0e4195bfc663bd3477a7d1959077
heejin9 <152829380+heejin9@users.noreply.github.com>
2024-01-25 09:19:27 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+from ANIMAL_INS 
+where NAME like 'Lucy' or NAME like 'Ella' or NAME like 'Pickle' or NAME like 'Rogan' or NAME like 'Sabrina'or NAME like 'Mitty'
+order by ANIMAL_ID
